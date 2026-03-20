67ba364d5c680c877d0519804329a14682456e57
ooliveiratg <menezesth16@gmail.com>
2026-03-20 20:10:48 -0300

    feat(cypress): Validating if nothing is entered in the inputs.

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -11,14 +11,21 @@ describe('template spec', () => {
     cy.get('#password').type('4DT')
     cy.contains('button', 'Entrar').click()
   })
-  it('should show error message', () => {
+
+  it('should show error email message', () => {
     cy.visit('http://localhost:3000')
-    cy.get('#email').type('J')
-    cy.get('#password').type('S')
+    cy.get('#email').type('4')
+    cy.get('#password').type('4')
     cy.contains('button', 'Entrar').click()
     cy.wait(3000)
     cy.contains('Hmm... esse email parece estar errado 🤔')
-    cy.contains()
-    
+  })
+
+  it('should show error password message', () => {
+cy.visit('http://localhost:3000')
+    cy.contains('button', 'Entrar').click()
+    cy.contains('Ei, não esqueça de digitar seu email!')
+    cy.contains('Você precisa de uma senha para entrar! 🔒')
+
   })
 })
